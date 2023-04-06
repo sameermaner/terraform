8b4797993223842b78fb9099f72702c29bcbf180
sameer <sameer@gmail.com>
2023-04-06 19:30:34 +0000

    second commit pubip

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "winvmrg"
+  location = "eastus"
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "pubip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
